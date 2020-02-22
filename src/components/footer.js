0c96724936aace07bad4ab0033dce52601147aba
Karol Moroz <k.j.moroz@gmail.com>
2020-02-23 01:59:15 +0800

    Set up layout

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => (
+  <footer className="footer">
+    <p>
+      © {new Date().getFullYear()} by Karol Moroz. Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>.
+    </p>
+  </footer>
+);
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,15 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import logo from "../images/logo-desktop.svg";
+import React from "react";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header className="header">
+    <img
+      src={logo}
+      alt="Diamond Way Buddhism Karma Kagyu Lineage Taiwan"
+      className="header__logo"
+    ></img>
   </header>
-)
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
