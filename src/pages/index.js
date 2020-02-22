bfee4699b428e304cf69b027f62f25e5d0ee028a
Karol Moroz <k.j.moroz@gmail.com>
2020-02-23 05:44:02 +0800

    Add buildspec.yml config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { Link } from "gatsby";
+import React, { useState } from "react";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-  </Layout>
-);
+const IndexPage = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <p>Count: {count}</p>
+      <button className="button" onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+      {count > 4 ? <h1>La papo mortis!</h1> : <h1>La papo vivas!</h1>}
+    </Layout>
+  );
+};
 
 export default IndexPage;
